04ccf733df4794f242bb4025ebcaba3ac366fdc8
Andhitia Rama <andhitia.r@gmail.com>
2017-07-02 17:34:55 +0700

    [8.0.1.0.0][l10n_id_taxform_faktur_pajak_masukan_enofa_export

---
+++ l10n_id_taxform_faktur_pajak_masukan_enofa_export/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Faktur Pajak Masukan E-Nofa Export",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_taxform_faktur_pajak_masukan",
+        "report_aeroo",
+    ],
+    "data": [
+        "reports/report_faktur_pajak_masukan_export_csv.xml",
+    ],
+    "installable": True,
+}
