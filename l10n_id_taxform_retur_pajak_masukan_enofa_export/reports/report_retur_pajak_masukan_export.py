51a748a322b09ef74711f78bcd2ef2bdb3522eb6
Andhitia Rama <andhitia.r@gmail.com>
2017-07-04 01:06:47 +0700

    [8.0.1.0.0][l10n_id_texform_retur_pajak_masukan_enofa_export]

---
+++ l10n_id_taxform_retur_pajak_masukan_enofa_export/reports/report_retur_pajak_masukan_export.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.report import report_sxw
+
+
+class Parser(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(Parser, self).__init__(cr, uid, name, context)
+        self.localcontext.update({})
