20efa02c00b823eb6cd530f43dd8f212632a6949
mike <viriyananda.michael@gmail.com>
2017-08-15 18:25:18 +0700

    Adding new module l10n_id_taxform_bukti_potong_pph_f113311_aeroo_report

---
+++ l10n_id_taxform_bukti_potong_pph_f113311_aeroo_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Report for Bukti Potong "
+            "PPH Final 4 Ayat 2(F.1.1.33.11)",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_taxform_bukti_potong_pph_f113311",
+        "report_aeroo"
+    ],
+    "data": [
+        "reports/report_bukti_potong_pph_f113311.xml",
+        "views/bukti_potong_pph_f113311_views.xml"
+    ],
+    "installable": True,
+}
