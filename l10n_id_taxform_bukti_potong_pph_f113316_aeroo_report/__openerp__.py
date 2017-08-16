a84a57c2bc25c59b2b2b31849c19096e61ef6564
mike <viriyananda.michael@gmail.com>
2017-08-16 17:18:32 +0700

    Update Version

--- l10n_id_taxform_bukti_potong_pph_f113316_aeroo_report/__openerp__.py
+++ l10n_id_taxform_bukti_potong_pph_f113316_aeroo_report/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Indonesia - Report for Bukti Potong "
             "PPH Final 4 Ayat 2(F.1.1.33.16)",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "license": "AGPL-3",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
