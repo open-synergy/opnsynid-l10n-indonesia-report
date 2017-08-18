bb73018ea781438c673ee0b406bbec421d57bfa6
mike <viriyananda.michael@gmail.com>
2017-08-18 09:55:39 +0700

    Update Version 8.0.1.2.0

--- l10n_id_taxform_bukti_potong_pph_f113306_aeroo_report/__openerp__.py
+++ l10n_id_taxform_bukti_potong_pph_f113306_aeroo_report/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Report for Bukti Potong PPH 23(F.1.1.33.06)",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "license": "AGPL-3",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
