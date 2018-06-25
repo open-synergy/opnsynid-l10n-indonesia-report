3ed8cda85893afee4b01436d13a8ccfaf5ad2f65
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-25 16:10:06 +0700

    [8.0.1.0.0][l10n_id_djbc_kb_lap_ppengeluaran_aeroo_report]

---
+++ l10n_id_djbc_kb_lap_pengeluaran_aeroo_report/wizards/date_range_selector.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class KBLapPengeluaranWizard(models.TransientModel):
+    _inherit = "l10n_id.kb_lap_pengeluaran_wizard"
+
+    @api.multi
+    def action_print_xls(self):
+        datas = {}
+        datas['form'] = self.read()[0]
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': "aeroo_reportLapPengeluaranXls",
+            'datas': datas,
+        }
+
+    @api.multi
+    def action_print_ods(self):
+        datas = {}
+        datas['form'] = self.read()[0]
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': "aeroo_reportLapPengeluaranOds",
+            'datas': datas,
+        }
