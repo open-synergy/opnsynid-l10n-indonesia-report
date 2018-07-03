4a6329bac7d508bb755d191cc136b09a01cc5220
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-03 14:00:15 +0700

    [8.0.1.1.0][l10n_id_djbc_kb_lap_mutasi_barang_jadi_aeroo_report]
    * Revision

--- l10n_id_djbc_kb_lap_mutasi_barang_jadi_aeroo_report/__openerp__.py
+++ l10n_id_djbc_kb_lap_mutasi_barang_jadi_aeroo_report/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Indonesia - Report for DJBC KB "
             "Laporan Mutasi Barang Jadi",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "license": "AGPL-3",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
