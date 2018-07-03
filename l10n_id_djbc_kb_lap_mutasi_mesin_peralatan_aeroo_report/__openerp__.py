e5a911dc4eac7860a3975269fc3e99cec4f51d66
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-03 13:47:40 +0700

    [8.0.1.0.0][l10n_id_djbc_kb_lap_mutasi_mesin_peralatan_aeroo_report]
    [8.0.1.0.0][l10n_id_djbc_kb_lap_mutasi_mesin_peralatan_aeroo_report] Revision

---
+++ l10n_id_djbc_kb_lap_mutasi_mesin_peralatan_aeroo_report/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Report for DJBC KB "
+            "Laporan Mutasi Mesin Peralatan",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_djbc_kb_lap_mutasi_mesin_peralatan",
+        "report_aeroo"
+    ],
+    "data": [
+        "reports/report_djbc_kb_lap_mutasi_mesin_peralatan.xml"
+    ],
+    "installable": True,
+}
