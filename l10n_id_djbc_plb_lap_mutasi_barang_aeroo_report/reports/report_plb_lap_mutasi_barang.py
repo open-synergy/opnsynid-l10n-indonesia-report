af1df4dab16c8f760457c2b35f94235fa0b312ad
mikevhe18 <viriyananda.michael@gmail.com>
2018-11-12 17:28:42 +0700

    [8.0.1.0.0][l10n_id_djbc_plb_lap_mutasi_barang_aeroo_report]

---
+++ l10n_id_djbc_plb_lap_mutasi_barang_aeroo_report/reports/report_plb_lap_mutasi_barang.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import time
+from openerp.report import report_sxw
+import pytz
+from datetime import datetime
+
+
+class Parser(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(Parser, self).__init__(cr, uid, name, context)
+        self.list_config = []
+        self.localcontext.update({
+            "time": time,
+            "get_data": self._get_data,
+            "get_date_start": self._get_date_start,
+            "get_date_end": self._get_date_end,
+        })
+
+    def set_context(self, objects, data, ids, report_type=None):
+        self.form = data["form"]
+        self.date_start = self.form["date_start"]
+        self.date_end = self.form["date_end"]
+        return super(Parser, self).set_context(objects, data, ids, report_type)
+
+    def _get_date_start(self):
+        return self._convert_datetime_utc(self.date_start)
+
+    def _get_date_end(self):
+        return self._convert_datetime_utc(self.date_end)
+
+    def _convert_datetime_utc(self, dt):
+        if dt:
+            obj_user = self.pool.get("res.users")
+            user = obj_user.browse(self.cr, self.uid, [self.uid])[0]
+            convert_dt = datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
+            if user.tz:
+                tz = pytz.timezone(user.tz)
+            else:
+                tz = pytz.utc
+            convert_utc = pytz.utc.localize(convert_dt).astimezone(tz)
+            format_utc = convert_utc.strftime("%d %B %Y")
+
+            return format_utc
+        else:
+            return "-"
+
+    def _get_data(self):
+        data = []
+        obj_data = self.pool.get(
+            "l10n_id.djbc_plb_lap_mutasi_barang")
+        no = 1
+
+        criteria = [
+            # ("tgl_penerimaan", ">=", self.date_start),
+            # ("tgl_penerimaan", "<=", self.date_end),
+            # ("warehouse_id", "in", self.warehouse_ids)
+        ]
+
+        data_ids = obj_data.search(self.cr, self.uid, criteria)
+
+        if data_ids:
+            context = {
+                "date_start": self.date_start,
+                "date_end": self.date_end
+            }
+            for data_id in obj_data.browse(self.cr, self.uid, data_ids, context):
+                product_id = data_id.product_id
+                product_name = product_id and product_id.name or "-"
+
+                uom_id = data_id.uom_id
+                uom_name = uom_id and uom_id.name or "-"
+                res = {
+                    "no": no,
+                    "kode_barang": data_id.kode_barang,
+                    "nama_barang": product_name,
+                    "sat": uom_name,
+                    "saldo_awal": data_id.saldo_awal,
+                    "pemasukan": data_id.pemasukan,
+                    "pengeluaran": data_id.pengeluaran,
+                    "penyesuaian": data_id.penyesuaian,
+                    "stock_opname": data_id.stock_opname,
+                    "saldo_akhir": data_id.saldo_akhir,
+                    "selisih": data_id.selisih,
+                    "keterangan": data_id.keterangan
+                }
+                data.append(res)
+                no += 1
+
+        return data
