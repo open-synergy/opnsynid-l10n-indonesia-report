c222db0d8879eccb640f585c1ea440adda5adde5
mikevhe18 <viriyananda.michael@gmail.com>
2018-11-12 17:29:08 +0700

    [8.0.1.0.0][l10n_id_djbc_plb_lap_pemasukan_aeroo_report]

---
+++ l10n_id_djbc_plb_lap_pemasukan_aeroo_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Report for DJBC PLB "
+            "Laporan Pemasukan",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_djbc_plb_lap_pemasukan",
+        "report_aeroo"
+    ],
+    "data": [
+        "reports/report_djbc_plb_lap_pemasukan.xml"
+    ],
+    "installable": True,
+}
