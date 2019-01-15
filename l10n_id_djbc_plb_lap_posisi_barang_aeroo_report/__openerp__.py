661833433302be2a8a3ff4391e63b7b1e8e686d9
Andhitia Rama <andhitia.r@gmail.com>
2019-01-16 00:42:09 +0700

    [8.0.1.0.0] l10n_id_djbc_plb_lap_posisi_barang_aeroo_report

---
+++ l10n_id_djbc_plb_lap_posisi_barang_aeroo_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Aeroo Report for DJBC PLB "
+            "Laporan Posisi Barang",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_djbc_plb_lap_posisi_barang",
+        "report_aeroo"
+    ],
+    "data": [
+        "reports/report_djbc_plb_lap_posisi_barang.xml"
+    ],
+    "installable": True,
+}
---
+++ l10n_id_djbc_plb_lap_posisi_barang_aeroo_report/wizards/cetak_lap_posisi_barang_plb.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class CetakLapPosisiBarangPlb(models.TransientModel):
+    _inherit = "l10n_id.cetak_lap_posisi_barang_plb"
+
+    @api.multi
+    def action_print_xls(self):
+        datas = {}
+        datas["form"] = self.read()[0]
+        return {
+            "type": "ir.actions.report.xml",
+            "report_name": "aeroo_reportPlbLapPosisiBarangXls",
+            "datas": datas,
+        }
+
+    @api.multi
+    def action_print_ods(self):
+        datas = {}
+        datas["form"] = self.read()[0]
+        return {
+            "type": "ir.actions.report.xml",
+            "report_name": "aeroo_reportPlbLapPosisiBarangOds",
+            "datas": datas,
+        }
