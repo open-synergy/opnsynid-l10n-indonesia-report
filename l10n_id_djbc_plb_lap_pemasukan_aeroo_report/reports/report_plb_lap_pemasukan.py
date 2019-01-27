4909d3ba6def9e93f81a77c25a6b5c65083d2cdf
Andhitia Rama <andhitia.r@gmail.com>
2019-01-27 17:34:50 +0700

    [LINT] l10n_id_djbc_plb_lap_pemasukan_aeroo_report

--- l10n_id_djbc_plb_lap_pemasukan_aeroo_report/reports/report_plb_lap_pemasukan.py
+++ l10n_id_djbc_plb_lap_pemasukan_aeroo_report/reports/report_plb_lap_pemasukan.py
@@ -13,6 +13,7 @@ _logger = logging.getLogger(__name__)
 
 class Parser(report_sxw.rml_parse):
 
+    # pylint: disable=locally-disabled, old-api7-method-defined
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.list_config = []
