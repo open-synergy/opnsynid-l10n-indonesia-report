29d7b9f57d1636b78702fbf6bd9b5c13300d0e41
mikevhe18 <viriyananda.michael@gmail.com>
2019-09-11 18:19:57 +0700

    [8.0.1.0.0][l10n_id_djbc_kite_lap_pemakaian_bahan_baku_report]

---
+++ l10n_id_djbc_kite_lap_pemakaian_bahan_baku_aeroo_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Report for DJBC KITE "
+            "Pemakaian Bahan Baku",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_djbc_kite_lap_pemakaian_bahan_baku",
+        "report_aeroo"
+    ],
+    "data": [
+        "reports/report_kite_pemakaian_bahan_baku.xml"
+    ],
+    "installable": True,
+}
