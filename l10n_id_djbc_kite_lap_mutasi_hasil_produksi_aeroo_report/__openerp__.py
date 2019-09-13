3d3c1faef8663053da6eb2497e517a8dbd21ea15
mikevhe18 <viriyananda.michael@gmail.com>
2019-09-13 12:57:12 +0700

    [8.0.1.0.0][l10n_id_djbc_kite_lap_mutasi_hasil_produksi_report]

---
+++ l10n_id_djbc_kite_lap_mutasi_hasil_produksi_aeroo_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Report for DJBC KITE "
+            "Mutasi Hasil Produksi",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_djbc_kite_lap_mutasi_hasil_produksi",
+        "report_aeroo"
+    ],
+    "data": [
+        "reports/report_kite_mutasi_hasil_produksi.xml"
+    ],
+    "installable": True,
+}
