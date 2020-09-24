17902662f31b293705fb798c8aea5fca37b6f498
Andhitia Rama <andhitia.r@gmail.com>
2020-09-24 09:49:05 +0700

    [l10n_id_djbc_plb_lap_pengeluaran_aeroo_report] 8.0.1.0.2
    * Add SO value on report

--- l10n_id_djbc_plb_lap_pengeluaran_aeroo_report/reports/report_plb_lap_pengeluaran.py
+++ l10n_id_djbc_plb_lap_pengeluaran_aeroo_report/reports/report_plb_lap_pengeluaran.py
@@ -84,6 +84,7 @@ class Parser(report_sxw.rml_parse):
                     "jumlah": data.jumlah,
                     "satuan": data.satuan,
                     "nilai": data.nilai,
+                    "nilai_so": data.nilai_so,
                     "pemilik_barang": data.pemilik_barang,
                     "kondisi_barang": "-"
                 }
